2f646ec30f354eb766ed6ba626bae4f21f4b7935
Abhishek Agarwal <abhishek.agarwal.work20@gmail.com>
2023-06-06 21:41:22 +0530

    set matrix zero better and optimal approach

--- Day 1 - Arrays 1/setMatrixZeroes.cpp
+++ Day 1 - Arrays 1/setMatrixZeroes.cpp
@@ -49,3 +49,91 @@ void setZeroes(vector<vector<int>> &matrix)
     }
   }
 }
+
+// Better approach
+void setZeroes(vector<vector<int>> &matrix)
+{
+  int rows = matrix.size();
+  int cols = matrix[0].size();
+
+  vector<int> rowMark(rows);
+  vector<int> colMark(cols);
+
+  for (int i = 0; i < rows; i++)
+  {
+    for (int j = 0; j < cols; j++)
+    {
+      if (matrix[i][j] == 0)
+      {
+        rowMark[i] = 1;
+        colMark[j] = 1;
+      }
+    }
+  }
+
+  for (int i = 0; i < rows; i++)
+  {
+    for (int j = 0; j < cols; j++)
+    {
+      if (rowMark[i] == 1 || colMark[j] == 1)
+      {
+        matrix[i][j] = 0;
+      }
+    }
+  }
+}
+
+void setZeroes(vector<vector<int>> &matrix)
+{
+  int rows = matrix.size();
+  int cols = matrix[0].size();
+
+  int c = 1;
+  for (int i = 0; i < rows; i++)
+  {
+    for (int j = 0; j < cols; j++)
+    {
+      if (matrix[i][j] == 0)
+      {
+        matrix[i][0] = 0;
+        if (j != 0)
+        {
+          matrix[0][j] = 0;
+        }
+        else
+        {
+          c = 0;
+        }
+      }
+    }
+  }
+
+  for (int i = 1; i < rows; i++)
+  {
+    for (int j = 1; j < cols; j++)
+    {
+      if (matrix[i][j] != 0)
+      {
+        if (matrix[0][j] == 0 || matrix[i][0] == 0)
+        {
+          matrix[i][j] = 0;
+        }
+      }
+    }
+  }
+
+  if (matrix[0][0] == 0)
+  {
+    for (int i = 0; i < cols; i++)
+    {
+      matrix[0][i] = 0;
+    }
+  }
+  if (c == 0)
+  {
+    for (int i = 0; i < rows; i++)
+    {
+      matrix[i][0] = 0;
+    }
+  }
+}
