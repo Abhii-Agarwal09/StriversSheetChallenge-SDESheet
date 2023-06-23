c552ea5508a27563cda20be8cd8cdd5febc62d08
Abhishek Agarwal <abhishek.agarwal.work20@gmail.com>
2023-06-23 16:19:21 +0530

    3sum optimized

--- Day 7 - Linked List and Arrays/3sum.cpp
+++ Day 7 - Linked List and Arrays/3sum.cpp
@@ -50,3 +50,44 @@ public:
     return ans;
   }
 };
+
+class Solution
+{
+public:
+  vector<vector<int>> threeSum(vector<int> &nums)
+  {
+    int size = nums.size();
+    vector<vector<int>> ans;
+    sort(nums.begin(), nums.end());
+    for (int i = 0; i < size; i++)
+    {
+      if (i > 0 && nums[i] == nums[i - 1])
+        continue;
+      int k = i + 1;
+      int j = size - 1;
+      while (k < j)
+      {
+        int sum = nums[i] + nums[k] + nums[j];
+        if (sum < 0)
+        {
+          k++;
+        }
+        else if (sum > 0)
+        {
+          j--;
+        }
+        else
+        {
+          ans.push_back({nums[i], nums[j], nums[k]});
+          k++;
+          j--;
+          while (k < j && nums[k] == nums[k - 1])
+            k++;
+          while (k < j && nums[j] == nums[j + 1])
+            j--;
+        }
+      }
+    }
+    return ans;
+  }
+};
